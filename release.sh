43c98ad832ac893854d5b7845dfd2a93d4a1ea0e
Loudbook <loudbooked@gmail.com>
2024-01-28 13:12:08 -0500

    Improve release script

---
+++ release.sh
@@ -0,0 +1,13 @@
+cargo build --release --target x86_64-unknown-linux-gnu
+cargo build --release --target aarch64-unknown-linux-gnu
+cargo build --release --target x86_64-pc-windows-gnu
+cargo build --release --target x86_64-apple-darwin
+cargo build --release --target aarch64-apple-darwin
+
+mkdir target/out
+
+cp target/x86_64-apple-darwin/release/minecraft-server-installer target/out/installer-osx-x86_64
+cp target/aarch64-apple-darwin/release/minecraft-server-installer target/out/installer-osx-aarch64
+cp target/x86_64-unknown-linux-gnu/release/minecraft-server-installer target/out/installer-linux-x86_64
+cp target/aarch64-unknown-linux-gnu/release/minecraft-server-installer target/out/installer-linux-aarch64
+cp target/x86_64-pc-windows-gnu/release/minecraft-server-installer.exe target/out/installer-windows-x86_64.exe
